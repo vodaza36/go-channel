7e4d8e858b13d7b1f7433ca06b10b829ac453872
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-06-27 23:16:16 +0200

    initial version

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+}
